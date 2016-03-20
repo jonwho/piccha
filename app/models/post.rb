cecb0acfd76cdd15279a570abb2ef13d2758c8c5
Jon Ho <lj080805@gmail.com>
2016-03-20 16:57:40 -0700

    associate posts with a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,9 @@
 class Post < ActiveRecord::Base
+  belongs_to :user
   validates :image, presence: true
 
   has_attached_file :image, styles: { :medium => "640x" }
 
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\z/
+  validates :user_id, presence: true
 end
